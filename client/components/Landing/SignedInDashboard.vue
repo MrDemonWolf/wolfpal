9c6b0ac3ebdd2511f233f6da66a3429585ffe889
Nathan Henniges <demonwolf@demonwolfdev.com>
2020-09-15 20:30:39 -0500

    Updated front-end.

---
+++ client/components/Landing/SignedInDashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="py-12 md:py-16">
+    <div class="container mx-auto px-6">
+      <div class="sm:grid sm:h-32 sm:grid-flow-row sm:gap-4 sm:grid-cols-3">
+        <div
+          class="flex flex-col justify-center px-4 py-4 bg-white border border-gray-300 rounded"
+        >
+          <div>
+            <p class="text-3xl font-semibold text-center text-gray-800">43</p>
+            <p class="text-lg text-center text-gray-500">Complated Goals</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
