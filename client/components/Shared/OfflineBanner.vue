bf0b3d371275ce71c0e05e24ce4232d4dcb27ac5
Nathan Henniges <demonwolf@demonwolfdev.com>
2020-10-08 10:53:49 -0500

    Added offline banner.

---
+++ client/components/Shared/OfflineBanner.vue
@@ -0,0 +1,10 @@
+<template>
+  <div
+    v-if="$nuxt.isOffline"
+    class="w-full p-3 mx-auto bg-red-400 dark:bg-red-500"
+  >
+    <div class="font-bold text-center text-white text-md font-roboto landing-2">
+      You are currently offline, Please reconnect to continue using this app.
+    </div>
+  </div>
+</template>
