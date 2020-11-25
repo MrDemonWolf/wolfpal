f2deeb89aa57a0242f28daf23c5e098bd991546a
Nathan Henniges <demonwolf@demonwolfdev.com>
2020-11-24 19:31:54 -0600

    API is ready for two factor now.

--- server/validation/auth/two-factor.js
+++ server/validation/auth/two-factor.js
@@ -4,14 +4,15 @@ const isEmpty = require('../../utils/is-empty');
 module.exports = data => {
   const errors = {};
 
+  data.token = !isEmpty(data.token) ? data.token : '';
   data.code = !isEmpty(data.code) ? data.code : '';
+
+  if (Validator.isEmpty(data.token)) {
+    errors.token = 'Token is required.';
+  }
 
   if (Validator.isEmpty(data.code)) {
     errors.code = 'Code is required.';
-  }
-
-  if (!Validator.isLength(data.code, { min: 6, max: 6 })) {
-    errors.code = 'Code must 6 characters long';
   }
 
   return {
