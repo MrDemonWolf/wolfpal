831ae73e81ebc2a7bd86346b335d9a8c88d81b75
Nathan Henniges <demonwolf@demonwolfdev.com>
2021-03-04 11:21:21 -0600

    WOLFPAL-11 Added code and errors to the account routes. (#51)

--- server/validation/account/change-password.js
+++ server/validation/account/change-password.js
@@ -2,25 +2,30 @@ const Validator = require('validator');
 const isEmpty = require('../../utils/is-empty');
 
 module.exports = data => {
+  const codes = {};
   const errors = {};
 
   data.oldPassword = !isEmpty(data.oldPassword) ? data.oldPassword : '';
   data.newPassword = !isEmpty(data.newPassword) ? data.newPassword : '';
 
   if (Validator.isEmpty(data.oldPassword)) {
+    codes.oldPassword = 'REQUIRED';
     errors.oldPassword = 'Old Password is required.';
   }
 
   if (Validator.isEmpty(data.newPassword)) {
+    codes.newPassword = 'REQUIRED';
     errors.newPassword = 'New Password is required.';
   }
 
   if (!Validator.isLength(data.newPassword, { min: 8, max: 56 })) {
+    codes.newPassword = 'NOT_LONG_ENOUGH';
     errors.newPassword =
       'New Password must be between 8 and 56 characters long';
   }
 
   return {
+    codes,
     errors,
     isValid: isEmpty(errors)
   };
--- server/validation/account/disable-two-factor.js
+++ server/validation/account/disable-two-factor.js
@@ -2,16 +2,19 @@ const Validator = require('validator');
 const isEmpty = require('../../utils/is-empty');
 
 module.exports = data => {
-  let error = null;
+  const codes = {};
+  const errors = {};
 
   data.code = !isEmpty(data.code) ? data.code : '';
 
   if (Validator.isEmpty(data.code)) {
-    error = 'Code is required.';
+    codes.code = 'REQUIRED';
+    errors.code = 'Code is required.';
   }
 
   return {
-    error,
-    isValid: isEmpty(error)
+    codes,
+    errors,
+    isValid: isEmpty(errors)
   };
 };
