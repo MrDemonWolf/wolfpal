862bed87e747d92832130845d5193b390921a9a3
Nathan Henniges <demonwolf@demonwolfdev.com>
2021-03-05 20:13:26 -0600

    WOLFPAL-13 Added code and errors to the notifications routes.

--- server/validation/notifications/email.js
+++ server/validation/notifications/email.js
@@ -2,21 +2,24 @@ const Validator = require('validator');
 const isEmpty = require('../../utils/is-empty');
 
 module.exports = data => {
-  // eslint-disable-next-line prefer-const
-  let errors = {};
+  const codes = {};
+  const errors = {};
 
   data.weeklyGoals = !isEmpty(data.weeklyGoals)
     ? data.weeklyGoals.toString()
     : '';
 
   if (Validator.isEmpty(data.weeklyGoals)) {
+    codes.weeklyGoals = 'REQUIRED';
     errors.weeklyGoals = 'Weekly Goals is required.';
   }
 
   if (!Validator.isBoolean(data.weeklyGoals)) {
+    codes.weeklyGoals = 'BOOLEAN';
     errors.weeklyGoals = 'Weekly Goals must be a boolean';
   }
   return {
+    codes,
     errors,
     isValid: isEmpty(errors)
   };
