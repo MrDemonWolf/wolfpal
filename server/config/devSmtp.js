d8811e63efcdd5f0d45e5a3729cb20570da06841
Nathan Henniges <demonwolf@demonwolfdev.com>
2021-03-22 11:35:13 -0500

    [WOLFPAL-47] Added the ability to add your own port and ip for devSMTP in the .env

--- server/config/devSmtp.js
+++ server/config/devSmtp.js
@@ -1,7 +1,8 @@
 const nodemailer = require('nodemailer');
 
 module.exports = nodemailer.createTransport({
-  port: 25,
+  port: process.env.DEV_SMTP_PORT || 587,
+  ip: process.env.DEV_SMTP_IP || 'localhost',
 
   secure: false,
   tls: {
